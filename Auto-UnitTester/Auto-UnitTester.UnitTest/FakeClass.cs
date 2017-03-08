b3ae950f53ac0780cea8eeadea9e5d94ac66f940
vparmar <vparmar@MIC101>
2017-03-08 13:18:21 +0530

    unit test project added to test auto tester

---
+++ Auto-UnitTester/Auto-UnitTester.UnitTest/FakeClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Auto_UnitTester.UnitTest
+{
+    class FakeClass
+    {
+        public void FakeFunction()
+        {
+            Console.WriteLine("Hello i am fake function");
+        }
+        public void FakeFunctionWithString(string value)
+        {
+            var substring = value.Substring('-');
+            Console.WriteLine(substring);
+        }
+    }
+}
--- Auto-UnitTester/Auto-UnitTester/Tester.cs
+++ Auto-UnitTester/Auto-UnitTester/Tester.cs
@@ -1,12 +1,39 @@
-﻿namespace Auto_UnitTester
+﻿using System;
+using System.Reflection;
+
+namespace Auto_UnitTester
 {
     public static class Tester
     {
-        public static bool Test(string value)
+        public static bool Test(Type type, string methodName)
         {
-            if (value == null) return false;
+            var methodInfo = ValidateInput(type, methodName);
 
-            return true;
+            var parameters = methodInfo.GetParameters();
+            var classInstance = Activator.CreateInstance(type, null);
+            if (parameters.Length == 0)
+            {
+                // This works fine
+                methodInfo.Invoke(classInstance, null);
+            }
+            else
+            {
+                object[] parametersArray = { "Hello" };
+                          
+                methodInfo.Invoke(classInstance, parametersArray);
+            }
+
+            return false;
+        }
+
+        private static MethodInfo ValidateInput(Type type, string methodName)
+        {
+            if (type == null) throw new Exception("Type cannot be null");
+            if (string.IsNullOrEmpty(methodName)) throw new Exception("Method name cannot be empty");
+
+            MethodInfo methodInfo = type.GetMethod(methodName);
+            if (methodInfo == null) throw new Exception("Method not available in provided type");
+            return methodInfo;
         }
     }
 }
